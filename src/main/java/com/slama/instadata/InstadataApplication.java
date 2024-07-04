6c1270b24e7e2637bbfecfec8c84dcd9b7e7e926
Sergio de Lamadrid <s.lamadrid2007@gmail.com>
2024-07-04 02:14:40 +0200

    Project Init

---
+++ src/main/java/com/slama/instadata/InstadataApplication.java
@@ -0,0 +1,13 @@
+package com.slama.instadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstadataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstadataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slama/instadata/InstadataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slama.instadata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstadataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
